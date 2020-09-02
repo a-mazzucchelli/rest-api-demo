67b76bf2543c19b1b8b4936a7ff5417d0d00b453
Alessandro Mazzucchelli <mazzucchelli.work@gmail.com>
2020-09-02 18:36:36 +0200

    added get and post routes

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 app.get('/', (req, res) => {
-  res.send('We are in home');
+  res.send('We are on home');
 });
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('connected to DB!')
+);
 
 app.listen(3000, () => console.log('app running on port 3000'));
